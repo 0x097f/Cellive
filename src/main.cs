5708fe2500445d7b98822ed6340a49f81057f943
0x097f <1120151209@qq.com>
2026-09-22 20:46:20 +0800

    ADD LICENSE AND MAIN
    ADD LICENSE AND MAIN AND AND AND AND AND  I LOVE BFDI

---
+++ src/main.cs
@@ -0,0 +1,11 @@
+/*Cellive : Copyright 2026 0x097f
+  License : SEE LICENSE
+  
+  Using iced : github.com/icedland/iced
+     License : github.com/icedland/iced/blob/master/LICENSE.txt 
+namespace Cellive
+{
+    public class main
+    {
+    }
+}
